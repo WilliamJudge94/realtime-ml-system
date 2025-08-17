93044bbdb0d7bb2d89aae2a2176ef540815c8db8
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2025-08-17 20:34:52 +0000

    MISE: making it so there is no mise idiomatic warning

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,5 +5,10 @@
 cd /workspaces/real-time-ml-system-cohort-4
 mise trust
 mise install
+
+# Configure mise to enable idiomatic version files for Python
+# This removes the deprecation warning and ensures .python-version files work
+mise settings add idiomatic_version_file_enable_tools python
+
 echo 'eval "$(/usr/local/bin/mise activate bash)"' >> ~/.bashrc
 source ~/.bashrc
