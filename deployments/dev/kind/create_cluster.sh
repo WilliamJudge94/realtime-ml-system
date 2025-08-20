5a74d60d84b9f82a828d340efd4d4bcab24ebe9b
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2025-08-20 02:05:40 +0000

    FEAT: automatically starting kafka and ui

--- deployments/dev/kind/create_cluster.sh
+++ deployments/dev/kind/create_cluster.sh
@@ -20,3 +20,13 @@ docker network create --subnet 172.100.0.0/16 ${CLUSTER_NAME}-network
 # 4. Create the cluster
 echo "Creating the cluster..."
 KIND_EXPERIMENTAL_DOCKER_NETWORK=${CLUSTER_NAME}-network kind create cluster --config ./kind-with-portmapping.yaml
+
+# 5. Install Kafka
+echo "Installing Kafka..."
+chmod +x ./install_kafka.sh
+./install_kafka.sh
+
+# 6. Install Kafka UI
+echo "Installing Kafka UI..."
+chmod +x ./install_kafka_ui.sh
+./install_kafka_ui.sh
