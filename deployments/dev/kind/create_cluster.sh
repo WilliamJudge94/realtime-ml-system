3e148871e2e11861797739cd1e4bb45e322f0635
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2025-08-19 20:47:20 -0500

    FEAT: making the cluster name a variable

--- deployments/dev/kind/create_cluster.sh
+++ deployments/dev/kind/create_cluster.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
+# Remember to change the cluster name in the kind-with-portmapping.yaml file
+CLUSTER_NAME="rwml-34fa"
+
 # Steps:
 
 # 1. Delete the cluster (if it exists, otherwise it will fail)
 echo "Deleting the cluster..."
-kind delete cluster --name rwml-34fa
+kind delete cluster --name $CLUSTER_NAME
 
 # 2. Delete the docker network (if it exists, otherwise it will fail)
 echo "Deleting the docker network..."
-docker network rm rwml-34fa-network
+docker network rm ${CLUSTER_NAME}-network
 
 # 3. Create the docker network
 echo "Creating the docker network..."
-docker network create --subnet 172.100.0.0/16 rwml-34fa-network
+docker network create --subnet 172.100.0.0/16 ${CLUSTER_NAME}-network
 
 # 4. Create the cluster
 echo "Creating the cluster..."
-KIND_EXPERIMENTAL_DOCKER_NETWORK=rwml-34fa-network kind create cluster --config ./kind-with-portmapping.yaml
+KIND_EXPERIMENTAL_DOCKER_NETWORK=${CLUSTER_NAME}-network kind create cluster --config ./kind-with-portmapping.yaml
