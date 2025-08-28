211356d2d5fc9d354109df321617b19fde444543
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2025-08-28 00:45:48 +0000

    FEAT: auto port forward grafana

--- deployments/dev/kind/install_grafana.sh
+++ deployments/dev/kind/install_grafana.sh
@@ -2,3 +2,7 @@
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm upgrade --install --create-namespace --wait grafana grafana/grafana --namespace=monitoring --values manifests/grafana-values.yaml
+
+
+export POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/name=grafana,app.kubernetes.io/instance=grafana" -o jsonpath="{.items[0].metadata.name}")
+kubectl --namespace monitoring port-forward $POD_NAME 3000
