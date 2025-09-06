ecf8d27abdd5bf01d596bea375d6ba2dc0d054a7
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2025-09-06 20:28:17 +0000

    BUG: reorder the script

--- deployments/dev/kind/install_mlflow.sh
+++ deployments/dev/kind/install_mlflow.sh
@@ -1,5 +1,5 @@
-# apply secret for minio access
-kubectl apply -f ./manifests/mlflow_minio_secret.yaml
-
 # install mlflow
 helm upgrade --install --create-namespace --wait mlflow oci://registry-1.docker.io/bitnamicharts/mlflow --namespace=mlflow --values ./manifests/mlflow-values.yaml
+
+# apply secret for minio access
+kubectl apply -f ./manifests/mlflow_minio_secret.yaml
